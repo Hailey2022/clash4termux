6b8eb36ef7ec16c555e999fd891133a86a066ae2
Your Name <you@example.com>
2022-08-12 15:48:56 -0700

    add clash

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ download_clash(){
     arch=$(uname -m)
     if [ "$arch" = "aarch64"]; then
         echo "architecture is aarch64"
-        curl -o ./c4t/clash --retry 5 -L https://github.com/
+        curl -o ./c4t/clash --retry 5 -L https://raw.githubusercontent.com/Hailey2022/clash4termux/main/clash
         chmod +x ./c4t/clash
     else
         echo "unsupported architecture"
