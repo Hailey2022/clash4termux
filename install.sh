5e85f31005134f1c31b436a202346b953b548b16
Hailey2022 <96413516+Hailey2022@users.noreply.github.com>
2022-08-12 00:43:17 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+download_clash(){
+    mkdir c4t
+    arch=$(uname -m)
+    if [ "$arch" = "aarch64"]; then
+        echo "architecture is aarch64"
+        curl -o ./c4t/clash --retry 5 -L https://github.com/
+        chmod +x ./c4t/clash
+    else
+        echo "unsupported architecture"
+        exit 1
+    fi
+}
+
+check_root(){
+    user=$(whoami)
+    if [ "$user" = "root" ]; then
+        echo "You are root"
+    else
+        echo "You are not root"
+        exit 1
+    fi
+}
+
+check_root
+download_clash
